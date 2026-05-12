fa701192689c5e46c0f977acd817c198adede8da
Aditya Hans <adityahans.17@gmail.com>
2026-05-12 11:49:24 +0530

    fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-slate-800 dark:text-slate-200 leading-tight">
             {{ __('Admin Analytics') }}
@@ -63,4 +63,4 @@
             </div>
         </div>
     </div>
-</x-admin-layout>
+</x-app-layout>
